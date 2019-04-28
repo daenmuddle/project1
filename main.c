fea818c209fc50629225de2d49cf6373c6ccc402
daenmuddle <49265846+daenmuddle@users.noreply.github.com>
2019-04-28 12:47:21 +1000

    Update main.c

--- main.c
+++ main.c
@@ -2,47 +2,70 @@
 #include<string.h>//stringlen()
 #include<stdlib.h>
 
-//Define my Caesar Cipher Function
-void caesarCipher(char* plainText, int key);
 
-int main(void){
-	float x;
-	printf("ent: ");
-	scanf("%f", &x);
-	int key = x;
-	
-	
+
+int main(void)
+{
+    int key;
 	char plainText[101];
-	
-	
-	
-	//Ask the user for the plain text to encrypt
-	printf("Please enter the plain text you want to encrypt: ");
-	
-	//Get the users text input and store it in plainText variable
-	fgets(plainText, sizeof(plainText), stdin);
-	
-	//Print the ciphered text
-	printf("The ciphered text is : ");
-	
-	//Print the ciphered text
-	caesarCipher(plainText, key);
-	
-	//system("pause"); // Comment out if you are not using windows OS
-}
-
-void caesarCipher(char* plainText, int key){
-	
+	int type;
 	int i=0;
 	int cypherValue;
 	char cypher;
 	
-	while( plainText[i] != '\0' && strlen(plainText)-1 > i){
+	printf("please choose from the following options:\n");
+	printf("to encrypt rotation cipher press 1\n");
+	printf("to encrypt substituion cipher press 2\n");
+	printf("to decrypt rotation cipher press 3\n");
+	printf("to decrypt substitution cipher press 4\n");
+	printf("selection :");
+	
+	scanf("%d", &type);
+	
+	if(type == 1)
+	{
+	    printf("please enter key: ");
+    	scanf("%d", &key);
+	
+    	//Ask the user for the plain text to encrypt
+        printf("Please enter the plain text you want to encrypt: ");
+	    scanf("%c", &plainText);
+	
+	
+	    //Get the users text input and store it in plainText variable
+	    fgets(plainText, sizeof(plainText), stdin);
+	    printf("The ciphered text is : ");
+		
+	    while( plainText[i] != '\0' && strlen(plainText)-1 > i)
+	    {
 		cypherValue = ((int)plainText[i] -97 + key) % 26 + 97;
 		cypher = (char)(cypherValue);
-		
-		printf("%c", cypher);
 		i++;
+		printf("%c", cypher);
+	    }
 	}
-	printf("\n");
+	
+	if(type == 3)
+	{
+	    printf("please enter key: ");
+    	scanf("%d", &key);
+	
+    	//Ask the user for the plain text to encrypt
+        printf("Please enter the ciphered text you want to decrypt: ");
+	    scanf("%c", &plainText);
+	
+	
+	    //Get the users text input and store it in plainText variable
+	    fgets(plainText, sizeof(plainText), stdin);
+	    printf("The decryped text is : ");
+		
+	    while( plainText[i] != '\0' && strlen(plainText)-1 > i)
+	    {
+		cypherValue = ((int)plainText[i] -97 - key) % 26 + 97;
+		cypher = (char)(cypherValue);
+		i++;
+		printf("%c", cypher);
+	    }
+	}
+	
 }
